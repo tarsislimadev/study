f6a2d44f1317f08bd60492a7f5f7192e70e63540
Tarsis de Lima <br.tmvdl@gmail.com>
2023-01-15 14:48:44 -0300

    Adding LOGGER library

---
+++ src/api/libs/logger/index.js
@@ -0,0 +1,11 @@
+const info = (name, value) => console.info(name, value)
+
+const log = (name, value) => console.info(name, value)
+
+const error = (name, value) => console.info(name, value)
+
+module.exports = {
+  error,
+  info,
+  log,
+}
