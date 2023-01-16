c7b18f51df448378451a5a8a560ce042055c26dd
Tarsis de Lima <br.tmvdl@gmail.com>
2023-01-16 17:27:03 -0300

    Fixing app.js file

--- src/api/app.js
+++ src/api/app.js
@@ -1,17 +1,35 @@
 const logger = require('./libs/logger')
-
+const db = require('./libs/database')
 const app = require('./router')
 
-app.post(['api', 'v1', 'forms'], (req, res) => {
-  logger.info('routes:app.post:api.v1.forms', { req, res })
+app.post(['api', 'v1', 'forms', 'list'], (req, res) => {
+  logger.info('routes:app.post:forms.list', { req, res })
 
-  return res.setJSON({ list: [] })
+  return res.setJSON({})
+})
+
+app.post(['api', 'v1', 'forms', 'create'], (req, res) => {
+  logger.info('routes:app.post:forms.create', { req, res })
+
+  return res.setJSON({})
 })
 
 app.post(['api', 'v1', 'forms', 'save'], (req, res) => {
-  logger.info('routes:app.post:api.v1.forms.save', { req, res })
+  logger.info('routes:app.post:forms.save', { req, res })
 
-  return res.json({ id: null })
+  return res.setJSON({})
+})
+
+app.post(['api', 'v1', 'questions', 'create'], (req, res) => {
+  logger.info('routes:app.post:questions.create', { req, res })
+
+  return res.setJSON(db.in('questions').new())
+})
+
+app.post(['api', 'v1', 'questions', 'list'], (req, res) => {
+  logger.info('routes:app.post:questions.list', { req, res })
+
+  return res.setJSON({})
 })
 
 module.exports = app
